91b9e689ee983b82a57075b885b51467c8840d36
nicolau-np <mr1Normaliii@gmail.com>
2024-02-01 10:23:23 +0000

    update

--- app/Http/Controllers/ViaturaController.php
+++ app/Http/Controllers/ViaturaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class EstudanteController extends Controller
+class ViaturaController extends Controller
 {
     /**
      * Display a listing of the resource.
