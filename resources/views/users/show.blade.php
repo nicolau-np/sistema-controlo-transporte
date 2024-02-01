7db84fcdbe28747d0d6079e7478167754ced08f2
nicolau-np <mr1Normaliii@gmail.com>
2024-02-01 13:27:55 +0000

    criando views

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">{{ $menu }}</h1>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+        <div class="col-md-12">
+
+        </div>
+    </div>
+</div>
+
+@endsection
