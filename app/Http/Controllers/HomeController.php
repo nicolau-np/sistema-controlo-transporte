21eebc8f7111802ff7b814818219f784bf403606
nicolau-np <mr1Normaliii@gmail.com>
2024-02-01 12:11:54 +0000

    adicionando primeiras paginas da view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,10 @@ class HomeController extends Controller
 
     public function index()
     {
-        
+        $title = 'SISTEMA DE CONTROLO DE TRANSPORTE';
+        $menu = 'Home';
+        $type = 'home';
+
+        return view('index', compact('title', 'menu', 'type'));
     }
 }
